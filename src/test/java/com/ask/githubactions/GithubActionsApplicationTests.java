fbdf7e00745175fbb436c4cab4d6627232fbf618
csh0034 <csh0034@gmail.com>
2022-03-13 14:04:05 +0900

    docs: workflows yml

--- src/test/java/com/ask/githubactions/GithubActionsApplicationTests.java
+++ src/test/java/com/ask/githubactions/GithubActionsApplicationTests.java
@@ -9,10 +9,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GithubActionsApplicationTests {
 
-  @EnabledIfEnvironmentVariable(named = "BUILD_SUCCESS", matches = "false")
+  @EnabledIfEnvironmentVariable(named = "BUILD_FORCE_FAIL", matches = "true")
   @Test
   void enabledFailTest() {
-    fail("BUILD_SUCCESS 환경변수 값이 false 이므로 실패처리");
+    fail("BUILD_FORCE_FAIL 환경변수 값이 true 이므로 실패처리");
   }
 
 }
